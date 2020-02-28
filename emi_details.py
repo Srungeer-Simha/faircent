67bd8c6ef1777b4122448c1b8dece5281470fd3b
Srungeer Simha <srungeer.simha@shell.com>
2020-02-28 16:12:04 +0530

    Calculated loan status based on emi history

--- emi_details.py
+++ emi_details.py
@@ -30,17 +30,25 @@ emi_data = pd.concat(emi_data, sort = False)
 emi_data["Investment Amount"] = emi_data["Investment Amount"].str.replace(",", "").astype(np.float32)
 emi_data["Rate"] = emi_data["Rate"].str.strip("%").astype(np.float32)
 emi_data["Tenure"] = emi_data["Tenure"].str.strip(" months").astype(np.int16)
+emi_data["EMI Date"] = pd.to_datetime(emi_data["EMI Date"], format = "%d-%m-%Y")
 
 emi_data.rename(columns = {"Tenure": "Tenure (months)", "Rate": "Rate %"}, inplace = True)
 
 ## Removing duplication of closed loans
 
+closed_loans = emi_data.loc[emi_data["EMI Status"] == "closed", "Loan Id"].unique()
+closed_dates = emi_data[(emi_data["Loan Id"].isin(closed_loans)) & (emi_data["EMI Status"] == "closed")].groupby("Loan Id")["EMI Date"].min()
+
+drop_rows = []
+for loan in closed_loans:
+    drop_rows.append(emi_data[(emi_data["Loan Id"] == loan) & (emi_data["EMI Date"] > closed_dates.loc[loan])].index)
+emi_data.drop(drop_rows[0], inplace = True, axis = 0)
 
 ## Aggregating data
 
 emi_status = emi_data.groupby(["Loan Id", "EMI Status"]).size().unstack(fill_value = 0).reset_index()
 
-emi_data = emi_data.groupby("Loan Id", as_index = False).agg({
+emi_agg = emi_data.groupby("Loan Id", as_index = False).agg({
                             "Borrower Name": "first",
                             "Investment Amount": np.mean,
                             "Rate %": np.mean,
@@ -49,9 +57,15 @@ emi_data = emi_data.groupby("Loan Id", as_index = False).agg({
                             "Principal Amount(INR)": np.sum,
                             "Interest Amount(INR)": np.sum})
 
-emi_data = pd.merge(emi_data, emi_status, on = "Loan Id", how = "left")
+emi_agg = pd.merge(emi_agg, emi_status, on = "Loan Id", how = "left")
 
 ## Detemining loan status
 
+emi_agg.loc[emi_agg["due"] > 1, "Status"] = "Default"
+emi_agg.loc[emi_agg["due"] == 1, "Status"] = "Delayed"
+emi_agg.loc[(emi_agg["due"] == 0) & (emi_agg["paid"] > 0), "Status"] = "Regular"
+emi_agg.loc[emi_agg["closed"] > 0, "Status"] = "Closed"
 
+#%% Exporting data
 
+emi_agg.to_excel(cur_dir.strip("EMI payments") + "EMI_status.xlsx", index = False)
